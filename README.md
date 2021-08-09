b4ad5c7a09eeb29d64583ecc12009c903e317a60
ianlee92 <ethian9227@gmail.com>
2021-08-09 11:29:26 +0900

    setting up the chart

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 https://github.com/gorhom/react-native-bottom-sheet
+https://github.com/rainbow-me/react-native-animated-charts
 
 expo start: start
 expo r -c: restart, clear cash
--- components/Chart.js
+++ components/Chart.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+} from "@rainbow-me/animated-charts";
+export const { width: SIZE } = Dimensions.get("window");
 const Chart = ({
   currentPrice,
   logoUrl,
@@ -11,36 +16,46 @@ const Chart = ({
 }) => {
   const priceChangeColor = priceChangePercentage7d > 0 ? "#34C759" : "#FF3B30";
   return (
-    <View style={styles.chartWrapper}>
-      {/* Titles */}
-      <View style={styles.titlesWrapper}>
-        <View style={styles.upperTitles}>
-          <View style={styles.upperLeftTitle}>
-            <Image source={{ uri: logoUrl }} style={styles.image} />
-            <Text style={styles.subtitle}>
-              {name} ({symbol.toUpperCase()})
+    <ChartPathProvider
+      data={{ points: sparkline, smoothingStrategy: "bezier" }}
+    >
+      <View style={styles.chartWrapper}>
+        {/* Titles */}
+        <View style={styles.titlesWrapper}>
+          <View style={styles.upperTitles}>
+            <View style={styles.upperLeftTitle}>
+              <Image source={{ uri: logoUrl }} style={styles.image} />
+              <Text style={styles.subtitle}>
+                {name} ({symbol.toUpperCase()})
+              </Text>
+            </View>
+            <Text style={styles.subtitle}>7d</Text>
+          </View>
+          <View style={styles.lowerTitles}>
+            <Text style={styles.boldTitle}>
+              ${currentPrice.toLocaleString("en-Us", { currency: "USD" })}
+            </Text>
+            <Text style={[styles.title, { color: priceChangeColor }]}>
+              {priceChangePercentage7d.toFixed(2)}%
             </Text>
           </View>
-          <Text style={styles.subtitle}>7d</Text>
         </View>
-        <View style={styles.lowerTitles}>
-          <Text style={styles.boldTitle}>
-            ${currentPrice.toLocaleString("en-Us", { currency: "USD" })}
-          </Text>
-          <Text style={[styles.title, { color: priceChangeColor }]}>
-            {priceChangePercentage7d.toFixed(2)}%
-          </Text>
+        <View style={styles.chartLineWrapper}>
+          <ChartPath height={SIZE / 2} stroke="black" width={SIZE} />
+          <ChartDot style={{ backgroundColor: "black" }} />
         </View>
       </View>
-    </View>
+    </ChartPathProvider>
   );
 };
 
 const styles = StyleSheet.create({
   chartWrapper: {
-    margin: 16,
+    marginVertical: 16,
   },
-  titlesWrapper: {},
+  titlesWrapper: {
+    marginHorizontal: 16,
+  },
   upperTitles: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -70,6 +85,9 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 18,
+  },
+  chartLineWrapper: {
+    marginTop: 40,
   },
 });
 
